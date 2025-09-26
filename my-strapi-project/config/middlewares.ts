b7c4887b6bf4926aa4114489f1aaaa6e162f0f35
Frog <Azhukova19842005@yandex.ru>
2025-09-27 02:45:13 +0300

    Добавлен CMS

---
+++ my-strapi-project/config/middlewares.ts
@@ -0,0 +1,31 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://127.0.0.1:5500'], // можно ['*'] для теста
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
